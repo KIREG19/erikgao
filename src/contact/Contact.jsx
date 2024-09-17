fa1e8c6e005b176d23bfee288345fca142ededcb
KIREG19 <garvin.gao@gmail.com>
2024-09-17 13:08:08 -0400

    update contact

--- src/contact/Contact.jsx
+++ src/contact/Contact.jsx
@@ -1,15 +1,33 @@
-import Stars from "./Stars";
-
-
-
+import GitHub from '../navbar/GitHub';
+import LinkedIn from '../navbar/LinkedIn';
+import Stars from './Stars';
 
 function Contact() {
   return (
-    <div className='relative h-100vh flex flex-col text-6vw items-center justify-center text-center'>
+    <div className='relative h-100vh flex flex-col text-6vw items-center justify-center'>
       <Stars />
       Contact
-      <div>
-        ...Stay tuned
+      <div className='m-12'>
+        <div className='flex my-6'>
+          <LinkedIn />
+          <a
+            href='https://www.linkedin.com/in/erikgaogg/'
+            target='_blank'
+            className='cursor-pointer text-50px mx-6'
+          >
+            linkedin.com/in/erikgaogg
+          </a>
+        </div>
+        <div className='flex my-6'>
+          <GitHub />
+          <a
+            href='https://github.com/KIREG19'
+            target='_blank'
+            className='cursor-pointer text-50px mx-6'
+          >
+            github.com/KIREG19
+          </a>
+        </div>
       </div>
     </div>
   );
